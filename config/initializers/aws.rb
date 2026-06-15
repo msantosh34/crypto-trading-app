3243499b5995af7406b4e9742186b23e1bce8950
msantosh34 <msantosh34@gmail.com>
2026-06-15 13:13:44 +0530

    Create aws.rb

---
+++ config/initializers/aws.rb
@@ -0,0 +1,10 @@
+# AWS S3 Configuration
+require 'aws-sdk-s3'
+
+Aws.config.update(
+  region: ENV['AWS_REGION'] || 'us-east-1',
+  credentials: Aws::Credentials.new(
+    ENV['AWS_ACCESS_KEY_ID'],
+    ENV['AWS_SECRET_ACCESS_KEY']
+  )
+)
